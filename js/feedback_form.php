00819b8d7bed2f82dc711bcc8a89ef20b1d1a76d
kongoriversafaris <146997775+kongoriversafaris@users.noreply.github.com>
2023-11-21 20:35:55 +0300

    Update feedback_form.php

--- js/feedback_form.php
+++ js/feedback_form.php
@@ -1,18 +1,19 @@
 <?php
 
 #Receive user input
-$email_address = $_POST['email_address'];
-$feedback = $_POST['feedback'];
+$message-name = $_POST['message-name'];
+$message = $_POST['message'];
+$message-email = $_POST['message-email'];
 
 #Filter user input
 function filter_email_header($form_field) {  
 return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
 }
 
-$email_address  = filter_email_header($email_address);
+$message-email  = filter_email_header($email_address);
 
 #Send email
-$headers = "From: $email_address";
+$headers = "From: $message-email";
 $sent = mail('abduljumamahrez@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
 #Thank user or notify them of a problem
