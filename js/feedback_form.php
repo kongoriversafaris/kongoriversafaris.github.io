b3d153dc4d8f23311f9c908973a9a12bba5c0322
kongoriversafaris <146997775+kongoriversafaris@users.noreply.github.com>
2023-11-21 19:52:24 +0300

    Update feedback_form.php

--- js/feedback_form.php
+++ js/feedback_form.php
@@ -13,7 +13,7 @@ $email_address  = filter_email_header($email_address);
 
 #Send email
 $headers = "From: $email_addressn";
-$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
+$sent = mail('abduljumamahrez@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
 #Thank user or notify them of a problem
 if ($sent) {
